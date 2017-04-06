f33de47f48302a1e6d202bdd8904bca01137edce
DRAJ <davidrajnus@gmail.com>
2017-04-06 18:27:21 +0800

    refactoring and test done

---
+++ test.rb
@@ -0,0 +1,8 @@
+require_relative 'app'
+
+cohort = Cohort.find(1)
+cohort[:name] = 'Best Cohort Ever'
+cohort.save
+
+# This re-queries the database, so we're checking that we actually saved the data as intended
+Cohort.find(1)[:name] == 'Best Cohort Ever'
